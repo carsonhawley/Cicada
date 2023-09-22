311d2bc7a3196800bec349df75734eedb4a54197
bluebonsai <68259108+CarsonHawley@users.noreply.github.com>
2023-09-21 20:30:12 -0400

    Initial Commit

---
+++ Cicada/Cicada.h
@@ -0,0 +1,18 @@
+//
+//  Cicada.h
+//  Cicada
+//
+//  Created by Carson Hawley on 8/30/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Cicada.
+FOUNDATION_EXPORT double CicadaVersionNumber;
+
+//! Project version string for Cicada.
+FOUNDATION_EXPORT const unsigned char CicadaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cicada/PublicHeader.h>
+
+
