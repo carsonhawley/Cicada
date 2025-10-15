67bfad1769f02bebc5aa36c3c9e382655a77bbf0
bluebonsai <68259108+carsonhawley@users.noreply.github.com>
2025-10-15 11:01:01 -0400

    bugfix - performance issues
    - fixed: a noticeable delay re-initializing the capture session after the host application returns to the foreground. SDK now resets capturePreviewLayer and clears captureSession outputs whenever stop() is called.

--- Cicada/Capture.swift
+++ Cicada/Capture.swift
@@ -90,6 +90,8 @@ public class Capture: NSObject, AVCaptureMetadataOutputObjectsDelegate {
     /// Signals capture events using haptic device feedback
     private var hapticFeedbackGenerator: HapticFeedbackGenerator?
     
+    private var sessionQueue: DispatchQueue
+    
     /// The current operating mode of the camera instance
     public private(set) var mode: Mode
     
@@ -147,6 +149,8 @@ public class Capture: NSObject, AVCaptureMetadataOutputObjectsDelegate {
         if let hapticStyle {
             self.hapticFeedbackGenerator = HapticFeedbackGenerator(style: hapticStyle)
         }
+        
+        sessionQueue = DispatchQueue(label: "com.cicada.sessionQueue")
     }
     
     /// Ensures device output can produce the requested metadata types
@@ -306,19 +310,26 @@ public class Capture: NSObject, AVCaptureMetadataOutputObjectsDelegate {
     }
     
     /// Stops scanning for codes
-    ///
     public func stop() {
         capturePreviewLayer?.removeFromSuperlayer()
+        capturePreviewLayer?.session = nil
+        capturePreviewLayer = nil
         
-        // prevent crash when calling stop before start
-        if captureSession?.isRunning == true {
-            DispatchQueue.global(qos: .userInitiated).async { [self] in
-                captureSession?.stopRunning()
-                toggleTorch(on: false)
+        if let session = self.captureSession {
+            if session.isRunning {
+                sessionQueue.async {
+                    session.outputs.forEach {
+                        session.removeOutput($0)
+                    }
+                    session.stopRunning()
+                }
+            } else {
+                print("Capture is already stopped")
             }
-        } else {
-            print("Capture is already stopped")
+            captureSession = nil
         }
+        toggleTorch(on: false)
+        
         removeOrientationDidChangeObserver()
     }
     
